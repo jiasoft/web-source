b59f16298ae6cb40d08e4d5de5bea2db862e0f8d
jiasoft <jiasoft@163.com>
2023-12-11 16:35:45 +0800

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 CURRENT_DIR=$(pwd)
 
-
 APPS=("$CURRENT_DIR" "$CURRENT_DIR/packages/application/app-vue-framework" "$CURRENT_DIR/packages/application/app-vue")
 
 for app in "${APPS[@]}"
@@ -12,9 +11,10 @@ do
     cd -
 done
 
-APPSBUILD=("$CURRENT_DIR/packageschart-attribute" "$CURRENT_DIR/chart-config" "$CURRENT_DIR/data-source-edit")
 
-for app in "${APPS[@]}"
+APPS_BUILD=("$CURRENT_DIR/packages/chart-attribute" "$CURRENT_DIR/packages/chart-config" "$CURRENT_DIR/packages/data-source-edit" "$CURRENT_DIR/packages/grid-layout-wc")
+
+for app in "${APPS_BUILD[@]}"
 do
     cd  $app
     npm run build
