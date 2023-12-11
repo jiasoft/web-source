9532be91b877648ea98f3b183b2378fd58d58c97
jiasoft <jiasoft@163.com>
2023-12-11 15:46:14 +0800

    update

--- install.bat
+++ install.bat
@@ -3,14 +3,14 @@
 set CURRENT_DIR=%cd%
 
 
-@REM set APPS=%CURRENT_DIR% %CURRENT_DIR%/packages/application/app-vue-framework %CURRENT_DIR%/packages/application/app-vue
+set APPS=%CURRENT_DIR% %CURRENT_DIR%/packages/application/app-vue-framework %CURRENT_DIR%/packages/application/app-vue
 
-@REM for %%A in (%APPS%) do (
-@REM     cd /D %%A
-@REM     npm install
-@REM     cd /D %CURRENT_DIR%
-@REM     echo %%A end
-@REM )
+for %%A in (%APPS%) do (
+    cd /D %%A
+    npm install
+    cd /D %CURRENT_DIR%
+    echo %%A end
+)
 
 set APPSDIR=%CURRENT_DIR%/packages/chart-attribute %CURRENT_DIR%/packages/chart-config %CURRENT_DIR%/packages/data-source-edit
 
